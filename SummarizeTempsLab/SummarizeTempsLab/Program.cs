e6deab14c445daeb03b0acd18e2cc18237bce274
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-02-11 16:32:21 -0600

    added while loop

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -9,70 +9,123 @@ namespace SummarizeTempsLab
         {
             //Write out prompt to the console
             Console.WriteLine("*** TEMPERATURE ANALYSIS ***");
+
             Console.WriteLine("What is the file name?");
 
             // Read the filename from the console
             string filePath = Console.ReadLine();
 
+            int loopNumber = 1;
+
             // Test whether file exists
             if (File.Exists(filePath))
             {
 
-                // If file exists, ask user to provide temperature reading for threshold
                 Console.WriteLine("This program will calculate the number of temperatures above and below a threshold temperature");
-                Console.WriteLine("What is the threshold?");
-                string input = Console.ReadLine();
-                int tempThreshold = int.Parse(input);
 
-                // Open the data file
-                using (StreamReader sr = File.OpenText(filePath))
+                bool wantsToContinue = true;
+                while (wantsToContinue == true)
                 {
-                    
-                    string line = "";
-                    int totalTemp = 0;
-                    int amountTemp = 0;
-                    int aboveTemp = 0;
-                    int belowTemp = 0;
 
+                    // If file exists, ask user to provide temperature reading for threshold
+                    Console.WriteLine("What is the threshold?");
+                    string input = Console.ReadLine();
+                    int tempThreshold = int.Parse(input);
 
-                    // Read each line of the data file into a string
-                    // While a line is not null
-                    while ((line = sr.ReadLine()) != null)
+                    // Open the data file
+                    using (StreamReader sr = File.OpenText(filePath))
                     {
-                        // Parse each string
-                        int temp = int.Parse(line);
 
-                        // Add each string together in totalTemp
-                        totalTemp = totalTemp + temp;
+                        string line = "";
+                        int totalTemp = 0;
+                        int amountTemp = 0;
+                        int aboveTemp = 0;
+                        int belowTemp = 0;
 
-                        // For each string add +1 to number of temperatures
-                        amountTemp++;
 
-                        // If the current temperature value >= threshold 
-                        //      Increment "above" counter by one
-                        if (temp >= tempThreshold)
+                        // Read each line of the data file into a string
+                        // While a line is not null
+                        while ((line = sr.ReadLine()) != null)
                         {
-                            aboveTemp++;
+                            // Parse each string
+                            int temp = int.Parse(line);
+
+                            // Add each string together in totalTemp
+                            totalTemp = totalTemp + temp;
+
+                            // For each string add +1 to number of temperatures
+                            amountTemp++;
+
+                            // If the current temperature value >= threshold 
+                            //      Increment "above" counter by one
+                            if (temp >= tempThreshold)
+                            {
+                                aboveTemp++;
+                            }
+                            // Else (temperature is below) increment
+                            //      Increment "below" counter by one
+                            else
+                            {
+                                belowTemp++;
+                            }
+
                         }
-                        // Else (temperature is below) increment
-                        //      Increment "below" counter by one
+                        string resultNumber = "*** Result Number " + loopNumber + " ***";
+                        string thresholdString = "There are " + aboveTemp + " temperatures above the threshold and " + belowTemp + " temperatures below the threshold.";
+                        string averageString = "The average temperature is " + totalTemp / amountTemp + " degrees.";
+
+                        string[] results = { resultNumber, thresholdString, averageString };
+
+                        Array.ForEach(results, Console.WriteLine);
+
+
+                        Console.WriteLine("Would you like to save the results to a file? Enter yes or no");
+                        string saveResults = Console.ReadLine();
+
+                        if (saveResults == "yes")
+                        {
+                            Console.WriteLine("What would you like to name the file?");
+                            string resultFileName = Console.ReadLine();
+
+                            string docPath = @"C:\Users\Samuel Harvey\Documents\GitHub\SummarizeTempsLab\SummarizeTempsLab\SummarizeTempsLab\Results";
+
+                            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, resultFileName + ".txt")))
+                            {
+                                foreach (string result in results)
+                                    outputFile.WriteLine(result);
+                            }
+
+                            Console.WriteLine("The results have been successfully saved to " + resultFileName + ".txt. This file can be found in the Results folder.");
+                        }
                         else
                         {
-                            belowTemp++;
+
                         }
-
                     }
-                    Console.WriteLine("There are " + aboveTemp + " temperatures above the threshold and " + belowTemp + " temperatures below the threshold.");
-                    Console.WriteLine("The average temperature is " + totalTemp/amountTemp + " degrees.");
-                }
 
-                
+
+                    Console.WriteLine("Do you want to calculate more results? Enter yes or no.");
+                    string inputyesno = Console.ReadLine();
+                    if (inputyesno == "yes")
+                    {
+                        loopNumber++;
+                    }
+                    else
+                    {
+                        wantsToContinue = false;
+                    }
+
+                }
             }
+
             // Else (file does not exist) tell user file does not exist
             else
             {
                 Console.WriteLine("That file cannot be found");
             }
+
+            Console.WriteLine("Exiting program...");
+
         }
     }
 }
